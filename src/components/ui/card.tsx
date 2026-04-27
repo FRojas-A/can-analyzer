f9cb44eb779a857a103952a8ee8661d248f09a87
Frank Rojas <frankrojas215@gmail.com>
2026-04-26 20:41:31 -0500

    refactor: remove React components, hooks, and styles

--- src/components/ui/card.tsx
+++
@@ -1,9 +0,0 @@
-const Card = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div className="border rounded-lg">
-            {children}
-        </div>
-    )
-}
-
-export default Card
